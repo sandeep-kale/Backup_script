7315d9bb59d5c48abbbb19b8b2f18473c3086b8c
Sandeep Kale <156534370+sandeep-kale@users.noreply.github.com>
2024-04-23 20:15:04 +0530

    Update backup_script.sh

--- backup_script.sh
+++ backup_script.sh
@@ -2,18 +2,26 @@
 #Version-02
 #Author- Sandeep Kale
 ##########The is Backup_script #########
+#!/bin/bash
 
-echo -e " \n ******** Hello! $(whoami) ********"
+# Print greeting message
+echo -e "\n ******** Hello! $(whoami) ********"
 echo " ================================ "
-# to take input from user
-echo -e " \n Get backup of New directory \n "
-read -p "Enter the Source_directory_name: " SDN
-read -p "Enter the Destination_directory_name: " DDN
 
+# Prompt user for input
+echo -e "\n Get backup of New directory \n "
+read -p "Enter the Source directory name: " SDN
+read -p "Enter the Destination directory name: " DDN
 
-# To create source_dir & backup_dir first
-sudo mkdir $(pwd)/$SDN
-sudo mkdir $(pwd)/$DDN
+# Check if source and destination directories already exist
+if [ -d "$SDN" ] || [ -d "$DDN" ]; then
+    echo "Source or destination directory already exists. Exiting."
+    exit 1
+fi
+
+# Create source and destination directories
+mkdir -p "$SDN"
+mkdir -p "$DDN"
 
 # Source directory to be backed up
 source_dir="$(pwd)/$SDN"
@@ -23,18 +31,19 @@ backup_dir="$(pwd)/$DDN"
 
 # Timestamp for backup filename
 time_stamp=$(date +"%Y%m%d-%H%M%S")
-echo " "
 
 # Create a backup archive
 backup_filename="$time_stamp.tar.gz"
 backup_filepath="${backup_dir}/${backup_filename}"
 
-echo -e "Creating backup of $SDN... \n  "
+echo -e "\nCreating backup of $SDN... \n"
 
-sudo tar -czvf "$backup_filepath" "$source_dir"
-echo " "
+# Create backup archive
+tar -czvf "$backup_filepath" "$source_dir" >/dev/null 2>&1
+
+# Check if backup was successful
 if [ $? -eq 0 ]; then
-	  echo "Backup successful! Archive saved to: $backup_filepath"
-  else
-          echo "Backup failed. Check for errors."
+    echo "Backup successful! Archive saved to: $backup_filepath"
+else
+    echo "Backup failed. Check for errors."
 fi
